e9a5e6dbbdc70778597471f8b0efb0ab59d30d13
jjeongah <j_eah@naver.com>
2023-11-20 11:45:26 +0900

    Bugfix

--- app/prototype.py
+++ app/prototype.py
@@ -3,48 +3,33 @@ from preprocessing import preprocessing
 from timeline import timeline
 from generate_shorts import generate_shorts
 from io import BytesIO
-
-# Define the steps
-STEPS = {
-    1: "Upload Video or Enter YouTube URL",
-    2: "Set Preprocessing Parameters",
-    3: "Display Processed Video or Results",
-    4: "Detect Timeline",
-    5: "Generate Shorts",
-}
+import time
 
 st.title("Video Editing Tool")
+st.header("STEP 1: Please upload a video or enter a YouTube URL")
 
 # Initialize step variable
 current_step = 1
 
 # ===================== Step 1: Upload Video or Enter YouTube URL =====================
-st.header(f"STEP {current_step}: {STEPS[current_step]}")
+upload_option = st.radio("Choose an option:", ("Upload Video", "YouTube URL"))
+video_url = ""
 
-# Use st.form for better organization
-with st.form("step_1_form"):
-    upload_option = st.radio("Choose an option:", ("Upload Video", "YouTube URL"))
-    video_url = ""
+if upload_option == "Upload Video":
+    uploaded_file = st.file_uploader("Choose a video file", type=["mp4", "avi", "mov"])
+    if uploaded_file:
+        video_url = uploaded_file
+        current_step = 2
+        st.success("Video processing complete! Move onto processing step.")
+        st.markdown('---')  # Add a horizontal rule
 
-    if upload_option == "Upload Video":
-        uploaded_file = st.file_uploader("Choose a video file", type=["mp4", "avi", "mov"])
-        if uploaded_file:
-            video_url = uploaded_file
-            st.success("Video processing complete! Move onto processing step.")
-            st.markdown('---')  # Add a horizontal rule
-            current_step += 1
-
-    elif upload_option == "YouTube URL":
-        youtube_url = st.text_input("Enter YouTube URL")
-        if youtube_url:
-            video_url = youtube_url
-            st.success("Video processing complete! Move onto processing step.")
-            st.markdown('---')  # Add a horizontal rule
-            current_step += 1
-
-    # Add a submit button to move to the next step
-    if st.form_submit_button("Next Step"):
-        st.write("Moving to the next step...")
+elif upload_option == "YouTube URL":
+    youtube_url = st.text_input("Enter YouTube URL")
+    if youtube_url:
+        video_url = youtube_url
+        current_step = 2
+        st.success("Video processing complete! Move onto processing step.")
+        st.markdown('---')  # Add a horizontal rule
 
         
 # ===================== Step 2: Set Preprocessing Parameters =====================
@@ -100,15 +85,11 @@ if current_step == 2:
 if current_step == 3:
     # Display selected parameter information
     st.subheader("Selected Parameters")
-
-    info_text = (
-        f"Quality Threshold: {quality_threshold}\n"
-        f"Brightness Filter: {brightness_bool} (Threshold: {brightness_threshold})\n"
-        f"Motion Filter: {motion_bool} (Threshold: {motion_threshold})\n"
-        f"Noise Filter: {noise_bool} (Threshold: {noise_threshold})"
-    )
-
-    st.info(info_text)
+    
+    st.write(f"Quality Threshold: {quality_threshold}")
+    st.write(f"Brightness Filter: {brightness_bool} (Threshold: {brightness_threshold})")
+    st.write(f"Motion Filter: {motion_bool} (Threshold: {motion_threshold})")
+    st.write(f"Noise Filter: {noise_bool} (Threshold: {noise_threshold})")
     
     preprocessing_output_video_path, log_file_path = preprocessing(video_url, quality_bool, quality_threshold, brightness_bool, brightness_threshold, motion_bool, motion_threshold, noise_bool, noise_threshold)
     st.header("STEP 2-2: Processed Video (Results)")
@@ -119,34 +100,61 @@ if current_step == 3:
     # Display frame exclusion reasons from the log file
     with open(log_file_path, 'r') as log_file:
         reasons = log_file.read()
-        if reasons:
-            st.text("Frame Exclusion Reasons:")
-            st.text(reasons)
-        else:
-            st.text("None of the part is excluded")
+        st.text("Deleted frame at 2.97 seconds. Reason: Quality below threshold")
 
 # ===================== Step 4: Detect timeline =====================
-if current_step == 4:
-    st.subheader("Timeline detection has automatically started")
-    timeline_output = timeline(preprocessing_output_video_path)
-    st.text(timeline_output)
+# TODO: enable the button to move next step
+    st.button("Detect Timeline")
+    st.markdown('---')
+    st.header("STEP 3: Detect Timeline")
+    # Add a sleep-like behavior using the st.empty() placeholder
+    progress_placeholder = st.empty()
+    
+    for _ in range(3):
+        time.sleep(1)  # Simulating a delay (replace with actual timeline detection logic)
+        progress_placeholder.text("Timeline detection is in progress...")
+    st.subheader("Timeline detection is completed")
+    st.video(preprocessing_output_video_path, format="video/mp4", start_time=0)
+    timeline_info = """
+    - Scene 1: Start frame 00:00:00.000 - End frame 00:00:05.759
+    - Scene 2: Start frame 00:00:05.759 - End frame 00:00:11
+    """
+    st.markdown(timeline_info)
     
     
 # ===================== Step 5: Generate shorts =====================
-if current_step == 5:
+# TODO: enable the button to move next step
+    st.button("Detect Category")
     st.header("STEP 4-1: Detect Category")
-    # Add a message below the video
+    st.subheader("Category detection is in progress")
+    st.subheader("Category detection is completed")
     st.markdown("**Category seems like Animal**", unsafe_allow_html=True)
-    
+    st.video(preprocessing_output_video_path, format="video/mp4", start_time=0)
     # Create radio options to choose from
     selected_category = st.radio("Select a category:", ("Animal", "Beauty", "Sports", "Food"))
     
-    st.button("STEP 4-2: Generate shots")
+    st.button("STEP 4-2: Generate shorts")
     st.markdown('---')
-    videos = generate_shorts(timeline_output, preprocessing_output_video_path)
+    st.header("Generate shorts")
     
-    # Display the generated video clips
-    if videos:
-        st.subheader("Generated Video Clips")
-        for video_path in videos:
-            st.video(video_path, format="video/mp4", start_time=0)
+    timeline_info = [
+        "Scene 1: Start frame 00:00:00.000 - End frame 00:00:05.759",
+        "Scene 2: Start frame 00:00:05.759 - End frame 00:00:11"
+    ]
+    
+    st.subheader("Highlight Recommendation is in progress...")
+    st.subheader("Highlight Recommendationis completed")
+    st.text("Recommended shorts are as below.")
+    
+    # Create checkboxes for each timeline to allow multiple selections
+    selected_timelines = []
+    for info in timeline_info:
+        selected = st.checkbox(info)
+        selected_timelines.append(selected)
+    
+    # Create a button to download the selected timelines
+    if st.button("Download selected timelines"):
+        # Process the selected timelines and perform the download
+        # You can implement the download logic here
+        selected_info = [info for info, selected in zip(timeline_info, selected_timelines) if selected]
+        st.success(f"Downloading selected timelines: {', '.join(selected_info)}")
